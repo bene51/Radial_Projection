162edfb84d2024e2a684aaa47f86a84fa008a1b7
Benjamin Schmid <bene.schmid@gmail.com>
2014-07-01 11:06:40 +0200

    Try harder to resample also unfused data.

--- src/main/java/projection/processing/ResampleSMP.java
+++ src/main/java/projection/processing/ResampleSMP.java
@@ -7,6 +7,7 @@ import ij.plugin.PlugIn;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 
 import javax.vecmath.Point3f;
@@ -44,6 +45,10 @@ public class ResampleSMP extends TimelapseProcessor implements PlugIn {
 	private int nLayers;
 	private IndexedTriangleMesh sphere;
 	private int[] indices;
+
+	// used if resampling unfused data:
+	private int[][] usedVertexIndices = null;
+	private int[] angleNames = null;
 
 	public void prepareResampling(File inputdir, File outputdir, int nLayers, int levels) throws IOException {
 		this.inputdir = inputdir;
@@ -90,31 +95,61 @@ public class ResampleSMP extends TimelapseProcessor implements PlugIn {
 		for(int j = 0; j < indices.length; j++)
 			overlaynew[j] = overlayold[indices[j]];
 		SphericalMaxProjection.saveIntData(overlaynew, new File(outputdir, "contributions.vertices").getAbsolutePath());
+
+
+
+		String[] angleFiles = inputdir.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".mat");
+			}
+
+		});
+		if(angleFiles != null && angleFiles.length > 0) {
+			Arrays.sort(angleFiles);
+			int nAngles = angleFiles.length;
+			this.usedVertexIndices = new int[nAngles][];
+
+			for(int aIndex = 0; aIndex < nAngles; aIndex++) {
+				File f = new File(inputdir, angleFiles[aIndex].replace(".mat", ".indices"));
+				usedVertexIndices[aIndex] = MaximumProjector.loadVertexIndices(f);
+			}
+		}
 	}
 
 	@Override
 	public void processTimepoint(final int tp) throws IOException {
-		String[] files = inputdir.list(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String f) {
-				return f.startsWith(String.format("tp%04d", tp)) && f.endsWith(".vertices");
+		int nAngles = usedVertexIndices != null ? usedVertexIndices.length : 1;
+
+		for(int a = 0; a < nAngles; a++) {
+			for(int l = 0; l < nLayers; l++) {
+				String basename = usedVertexIndices == null ?
+						String.format("tp%04d_%02d", tp, l) :
+						String.format("tp%04d_%s_l%02d", tp, angleNames[a], l);
+				String name = basename + ".vertices";
+
+				File infile = new File(inputdir, name);
+				File outfile = new File(outputdir, name);
+				if(outfile.exists())
+					continue;
+
+				short[] maxima = null;
+				if(usedVertexIndices == null) {
+					maxima = smp.loadMaxima(infile.getAbsolutePath());
+				} else {
+					maxima = new short[sphere.nVertices];
+					MaximumProjector.loadVertices(infile, usedVertexIndices[a], maxima);
+				}
+
+				for(int j = 0; j < levels; j++)
+					smp.smooth(maxima);
+
+				short[] resampled = new short[sphere.nVertices];
+				for(int j = 0; j < indices.length; j++)
+					resampled[j] = maxima[indices[j]];
+
+				SphericalMaxProjection.saveShortData(resampled, outfile.getAbsolutePath());
 			}
-		});
-		for(String name : files) {
-			File infile = new File(inputdir, name);
-			File outfile = new File(outputdir, name);
-			if(outfile.exists())
-				continue;
-
-			short[] maxima = smp.loadMaxima(infile.getAbsolutePath());
-			for(int j = 0; j < levels; j++)
-				smp.smooth(maxima);
-
-			short[] resampled = new short[sphere.nVertices];
-			for(int j = 0; j < indices.length; j++)
-				resampled[j] = maxima[indices[j]];
-
-			SphericalMaxProjection.saveShortData(resampled, outfile.getAbsolutePath());
 		}
 
 		String name = String.format("tp%04d_%02d", tp, 0);
